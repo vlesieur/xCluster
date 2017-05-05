55ee85d9fc1d6eb110da2e86e9dae96e0912356e
groschatchauve <valentin.lesieur@gmail.com>
2017-05-05 16:04:21 +0200

    improve PHP starting

--- front/startPHP.bat
+++ front/startPHP.bat
@@ -1,6 +1,23 @@
 TITLE XCLUSTER_PHP
 SET mypath=%~dp0
 cd %mypath%angular-seed/app
-C:\wamp64\bin\php\php7.0.10\php.exe -S localhost:8000
+
+:start
+%SystemRoot%\System32\where.exe /q php
+IF ERRORLEVEL 1 (
+	echo "Veuillez renseigner l'emplacement du dossier contenant PHP dans la variable d'environnement du systeme PATH."
+    EXIT /B
+) ELSE (
+	echo --------------------
+	echo Configuration de PHP
+	echo --------------------
+	echo Vous pouvez utiliser la commande php -i pour verifier votre configuration globale de PHP
+	php -r "echo 'upload_max_filesize : '.ini_get('upload_max_filesize');"
+	echo .
+	echo ----------------
+	echo Demarrage de PHP
+	echo ----------------
+    php -S localhost:8000
+)
 @echo off
 pause
